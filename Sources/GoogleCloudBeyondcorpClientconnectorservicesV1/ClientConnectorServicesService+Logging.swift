f0f5a47b3fb1fe77e73691bb13fed7a262c60a71
Carlos O'Ryan <coryan@google.com>
2026-07-27 13:05:32 -0400

    chore: update librarian circa 2026-07-24 (#20)
    Update the librarian version. In this version the `gcp.artifact.id`
    logging attribute is set to the package name, as it should be.

--- Sources/GoogleCloudBeyondcorpClientconnectorservicesV1/ClientConnectorServicesService+Logging.swift
+++ Sources/GoogleCloudBeyondcorpClientconnectorservicesV1/ClientConnectorServicesService+Logging.swift
@@ -33,7 +33,7 @@ extension Clients {
 
     public init(_ inner: any ClientConnectorServicesServiceStub, logger: Logger) {
       var logger = logger
-      logger[metadataKey: "gcp.artifact.id"] = "GoogleCloudBeyondcorpClientconnectorservicesV1"
+      logger[metadataKey: "gcp.artifact.id"] = "google-cloud-beyondcorp-clientconnectorservices-v1"
       logger[metadataKey: "gcp.client.service"] = "beyondcorp"
       logger[metadataKey: "gcp.experimental.swift.client"] = "ClientConnectorServicesService"
       self.inner = inner
