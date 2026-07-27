1802ca2399f3b5debd26e114e142780880c38112
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudBeyondcorpClientconnectorservicesV1/ClientConnectorService.swift
+++ Sources/GoogleCloudBeyondcorpClientconnectorservicesV1/ClientConnectorService.swift
@@ -240,7 +240,7 @@ public struct ClientConnectorService: Codable, Equatable, GoogleCloudWkt._AnyPac
 
         /// Initialize from a string value.
         ///
-        /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+        /// If the value is unknown, this initializes to [`unknownStringValue`](doc:TransportProtocol/unknownStringValue(_:)).
         public init(stringValue: Swift.String) {
           switch stringValue {
           case "TRANSPORT_PROTOCOL_UNSPECIFIED": self = .unspecified
@@ -251,7 +251,7 @@ public struct ClientConnectorService: Codable, Equatable, GoogleCloudWkt._AnyPac
 
         /// Initialize from an integer value.
         ///
-        /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+        /// If the value is unknown, this initializes to [`unknownIntValue`](doc:TransportProtocol/unknownIntValue(_:)).
         public init(intValue: Int) {
           switch intValue {
           case 0: self = .unspecified
@@ -499,7 +499,7 @@ public struct ClientConnectorService: Codable, Equatable, GoogleCloudWkt._AnyPac
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:State/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "STATE_UNSPECIFIED": self = .unspecified
@@ -515,7 +515,7 @@ public struct ClientConnectorService: Codable, Equatable, GoogleCloudWkt._AnyPac
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:State/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
